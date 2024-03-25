922747b798c9bcd3edcda4f37c4917e336126d43
b18060225 <130069389+b18060225@users.noreply.github.com>
2024-03-25 17:40:16 +0800

    Update mutex.cc

--- sylar/mutex.cc
+++ sylar/mutex.cc
@@ -5,7 +5,7 @@
 namespace sylar {
 
 Semaphore::Semaphore(uint32_t count) {
-    if(sem_init(&m_semaphore, 0, count)) {
+    if(sem_init(&m_semaphore, 0, count)) {    //调用 sem_init() 函数初始化信号量
         throw std::logic_error("sem_init error");
     }
 }
@@ -14,14 +14,14 @@ Semaphore::~Semaphore() {
     sem_destroy(&m_semaphore);
 }
 
-void Semaphore::wait() {
+void Semaphore::wait() {    //等待信号量
     if(sem_wait(&m_semaphore)) {
         throw std::logic_error("sem_wait error");
     }
 }
 
 void Semaphore::notify() {
-    if(sem_post(&m_semaphore)) {
+    if(sem_post(&m_semaphore)) {    //调用 sem_post() 函数增加信号量的计数
         throw std::logic_error("sem_post error");
     }
 }
@@ -31,11 +31,11 @@ FiberSemaphore::FiberSemaphore(size_t initial_concurrency)
 }
 
 FiberSemaphore::~FiberSemaphore() {
-    SYLAR_ASSERT(m_waiters.empty());
+    SYLAR_ASSERT(m_waiters.empty());    // 确保在销毁时等待队列为空。
 }
 
 bool FiberSemaphore::tryWait() {
-    SYLAR_ASSERT(Scheduler::GetThis());
+    SYLAR_ASSERT(Scheduler::GetThis());    // 首先获取互斥量的锁
     {
         MutexType::Lock lock(m_mutex);
         if(m_concurrency > 0u) {
@@ -54,19 +54,19 @@ void FiberSemaphore::wait() {
             --m_concurrency;
             return;
         }
-        m_waiters.push_back(std::make_pair(Scheduler::GetThis(), Fiber::GetThis()));
+        m_waiters.push_back(std::make_pair(Scheduler::GetThis(), Fiber::GetThis())); // 如果大于0则减少并发度并直接返回；否则将当前协程加入等待队列，然后切换到其他协程执行。
     }
-    Fiber::YieldToHold();
+    Fiber::YieldToHold();    // 切换协程
 }
 
 void FiberSemaphore::notify() {
     MutexType::Lock lock(m_mutex);
     if(!m_waiters.empty()) {
-        auto next = m_waiters.front();
+        auto next = m_waiters.front();    //等待队列不为空，则取出下一个协程
         m_waiters.pop_front();
         next.first->schedule(next.second);
     } else {
-        ++m_concurrency;
+        ++m_concurrency;    // 增加并发度
     }
 }
 
